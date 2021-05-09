95939169fd5321f94c026498ef1c8b13c39d06d4
Srdjan294 <srdjanfilipovic991@gmail.com>
2021-05-09 17:42:27 +0200

    Button Validate, "bridge", to do find background, on enter validate mail

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 function validate() {
     const form = document.getElementById('form');
     const email = document.getElementById('email').value;
-    const pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/
+    const pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+    const button = document.getElementById('button')
 
     if (email.match(pattern)) {
         form.classList.add('valid')
@@ -14,5 +15,33 @@ function validate() {
         form.classList.remove('invalid')
         form.classList.remove('valid')
     }
+    if (email.match(pattern)){
+        button.style.visibility = "visible";
+    }else{
+        button.style.visibility = "hidden";
+    }
+}
+
+function change(){
+    document.body.style.background="green";
+    const form = document.getElementById('form');
+    const btnarea = document.getElementById('btnarea');
+    const btnarea2 = document.getElementById('btnarea2');
+    const button = document.getElementById('button');
+
+    form.style.visibility = "hidden";
+    btnarea.style.visibility = "visible";
+    btnarea2.style.visibility = "visible";
+    btnarea2.style.zIndex = "15";
+    btnarea.style.zIndex = "10";
+    button.style.zIndex = "5";
+}
+
+function stepGrass(){
+    document.body.style.background="red";
+}
+
+function stepArea2(){
+    document.body.style.background = "green";
 }
 
